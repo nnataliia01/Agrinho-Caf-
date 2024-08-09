90f632d44253536295a30c3d0ff5cead6e4fd5fe
nnataliia01 <natalia.manago@escola.pr.gov.br>
2024-08-09 12:16:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Obter referências aos botões de navegação
+const btnInicio = document.getElementById('btnInicio');
+const btnHistoria = document.getElementById('btnHistoria');
+const btnImportancia = document.getElementById('btnImportancia');
+const btnTecnologia = document.getElementById('btnTecnologia');
+const btnCuriosidades = document.getElementById('btnCuriosidades');
+const btnReceita = document.getElementById('btnReceita');
+
+// Adicionar eventos de clique aos botões para redirecionamento
+btnInicio.addEventListener('click', function() {
+    window.location.href = 'index.html'; // Altere para o caminho correto da página inicial
+});
+
+btnHistoria.addEventListener('click', function() {
+    window.location.href = 'historia.html'; // Altere para o caminho correto da página de história
+});
+
+btnImportancia.addEventListener('click', function() {
+    window.location.href = 'importancia.html'; // Altere para o caminho correto da página de importância
+});
+
+btnTecnologia.addEventListener('click', function() {
+    window.location.href = 'tecnologia.html'; // Altere para o caminho correto da página de tecnologia
+});
+
+btnCuriosidades.addEventListener('click', function() {
+    window.location.href = 'curiosidades.html'; // Altere para o caminho correto da página de tecnologia
+});
+
+btnReceita.addEventListener('click', function() {
+    window.location.href = 'receita.html'; // Altere para o caminho correto da página de receita
+});
